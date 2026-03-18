9801dc42ec4b6a3be28d87d680bffc761e940ebe
Tamás Nagy <ntamasalbert@gmail.com>
2026-03-18 14:23:18 +0100

    Task 4

--- airplane-app/src/app/components/airplane-list/airplane-list.component.ts
+++ airplane-app/src/app/components/airplane-list/airplane-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 import { TailNumberPipe } from '../../pipes/tail-number.pipe';
 import { AirplaneService } from '../../services/airplane.service';
 import { Airplane } from '../../models/airplane.model';
 
 @Component({
   selector: 'app-airplane-list',
-  imports: [TailNumberPipe, RouterLink],
+  imports: [TailNumberPipe, RouterLink, NgClass],
   templateUrl: './airplane-list.component.html',
 })
 export class AirplaneListComponent {
